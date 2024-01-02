15240b3f320374a200bbe6f8b1a9dca1cd0a2dd1
JEMYJYMAT <155440976+JEMYJYMAT@users.noreply.github.com>
2024-01-02 13:19:04 +0200

    Add files via upload

---
+++ rational_num.h
@@ -0,0 +1,25 @@
+//rational header file 
+
+using namespace std;
+#include <iostream>
+
+class rational_num
+{ private:
+	int numirator;
+	int donominator;
+public:
+	rational_num();
+	rational_num(int n,int d);
+	rational_num(int n);
+	rational_num operator +(rational_num r);
+	rational_num operator +(int r);
+	rational_num operator *(rational_num r);
+	void operator --();
+	rational_num operator -(rational_num r);
+	void getdata();
+
+
+
+
+};
+
