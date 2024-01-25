1234b8ab3940c44594dbe8e3f973d9cbb487635f
Ahmed Gamal <155440976+JEMYJYMAT@users.noreply.github.com>
2024-01-25 13:12:52 +0200

    Create rational_num.h

---
+++ rational_num.h
@@ -0,0 +1,34 @@
+/*
+ * RationalNumber.h
+ *
+ *  Created on: Dec 20, 2023
+ *      Author: Ahmed
+ */
+#ifndef rational_header
+#define raional_header
+
+using namespace std;
+#include <iostream>
+
+class rational_num
+{
+private:
+	int numirator;
+	int denominator;
+public:
+	friend ostream& operator <<(ostream& out, rational_num& r);
+	rational_num();
+	rational_num(int n, int d);
+	rational_num(int n);
+	rational_num operator +(rational_num r);
+	rational_num operator +(int r);
+	rational_num operator *(rational_num r);
+	void operator --();
+	rational_num operator -(rational_num r);
+	void getdata();
+
+
+
+
+};
+#endif rational
